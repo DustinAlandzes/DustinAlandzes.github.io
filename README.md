ba7b466dd79db5278fd05745b85a7e6073611e5c
Dustin Alandzes <fezf00@gmail.com>
2024-04-13 13:11:54 -0500

    Initial project setup

--- README.md
+++ README.md
@@ -3,3 +3,23 @@
 ## Resources
 https://pages.github.com/
 
+https://react.dev/
+
+https://nextjs.org/docs
+
+https://nextjs.org/docs/app/building-your-application/testing/jest
+
+https://jestjs.io/docs/getting-started
+
+
+https://threejs.org/
+
+https://docs.pmnd.rs/react-three-fiber/getting-started/introduction
+
+## Models
+
+https://market.pmnd.rs/model/macbook
+
+## Fonts
+
+https://www.jetbrains.com/lp/mono/
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import styles from "./page.module.css";
+import * as THREE from 'three'
+import React, {useRef} from "react";
+import { Canvas, useFrame, ThreeElements } from '@react-three/fiber';
+
+function Box(props: ThreeElements['mesh']) {
+  const meshRef = useRef<THREE.Mesh>(null);
+
+  useFrame((state, delta) => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += delta
+    }
+  })
+
+  return <mesh {...props} ref={meshRef} >
+    <boxGeometry args={[1, 1, 1]} />
+    <meshStandardMaterial color={'orange'} />
+  </mesh>;
+}
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <div>
+          <h1>Dustin Alandzes</h1>
+          <Canvas>
+            {/* <Stage> and <OrbitControls> from drei?*/}
+            <ambientLight intensity={Math.PI / 2} />
+            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+            <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+            <Box position={[0, 0, 0]} />
+          </Canvas>
+        </div>
+      </div>
+    </main>
+  );
+}
